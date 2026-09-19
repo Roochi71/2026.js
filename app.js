bc512d0795ea8d3d7529c2ca1c134b70f4b9630f
Roochi71 <morteza.shabanir@gmail.com>
2026-09-19 06:53:37 -0700

    Change GLTF model file from room3-small to room3-v1

--- app.js
+++ app.js
@@ -310,7 +310,7 @@ function applyCustomImage(art, url) {
 
 const loader = new THREE.GLTFLoader(loadingManager);
 loader.load(
-    'room3-small.glb',
+    'room3-v1.glb',
     (gltf) => {
         const model = gltf.scene;
         scene.add(model);
