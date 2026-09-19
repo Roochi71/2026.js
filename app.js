c7b31e879885049664b08f53ca32f2d486d4f936
Roochi71 <morteza.shabanir@gmail.com>
2026-09-19 11:27:24 -0700

    Fix bottom position for mobile view and ensure newline

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ function showScrollHint() {
         </style>
         <div style="
             position: fixed;
-            bottom: ${isMobile ? 100 : 30}px;
+            bottom: 30px;
             right: 30px;
             background: rgba(0, 0, 0, 0.85);
             color: #fff;
