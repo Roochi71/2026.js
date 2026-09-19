5b3dfc9763e96f9cd1442f24d33cb1cdd0de6341
Roochi71 <morteza.shabanir@gmail.com>
2026-09-19 11:29:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ function showScrollHint() {
         </style>
         <div style="
             position: fixed;
-            bottom: 30px;
+            bottom: 50px;
             right: 30px;
             background: rgba(0, 0, 0, 0.85);
             color: #fff;
